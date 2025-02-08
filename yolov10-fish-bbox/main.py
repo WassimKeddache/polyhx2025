862163891b36edcd25107f094d5f653c54e8b47c
HearthWell <adam.el-houssami@polymtl.ca>
2025-02-08 17:41:29 -0500

    added basic bbox detector with weights tflite

---
+++ yolov10-fish-bbox/main.py
@@ -0,0 +1,36 @@
+import argparse
+from typing import List
+
+import torch
+import cv2
+
+from inference import YOLOInferenceTorch, YOLOResult
+
+def convert_model_onnx(model: torch.nn.Module, input: torch.Tensor):
+    torch.onnx.export(model, input, "output/model.onnx", input_names=["img"], output_names=["bbox"], opset_version=12)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="An example of argparse.")
+    parser.add_argument('--input', type=str, required=True, help="Input file path")
+
+    args = parser.parse_args()
+
+    inferer = YOLOInferenceTorch("model.ts")
+
+    im = cv2.imread(args.input)
+    print(f"IMAGE SHAPE: {im.shape}")
+    input = im.copy()
+
+    results: List[YOLOResult] = inferer.predict(input)
+    print(results)
+
+    current = results[0]
+    for bbox in current:
+        bbox.draw_box(im)
+
+    cv2.imwrite("output.png", im)
+
+    input_imgs, params = inferer.preprocess([input])
+    print(f"INPUT SHAPE: {input_imgs[0].shape}")
+
+    #convert_model_onnx(inferer.model, input_imgs)
