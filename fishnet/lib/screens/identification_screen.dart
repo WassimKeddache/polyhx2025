3cb6c7229ab2065cde4adf6b6fdd51e70372cc90
Wassim Keddache <keddachew@gmail.com>
2025-02-09 01:27:49 -0500

    fonctionne 1/4

--- fishnet/lib/screens/identification_screen.dart
+++ fishnet/lib/screens/identification_screen.dart
@@ -2,8 +2,8 @@ import 'dart:io'; // Pour manipuler les fichiers
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:permission_handler/permission_handler.dart';
-import '../services/classifier_service.dart';  // Importer le service de classification
-import '../widget/bottom_nav_bar.dart';  // Importer le widget de la barre de navigation
+import '../services/classifier_service.dart'; // Importer le service de classification
+import '../widget/bottom_nav_bar.dart'; // Importer le widget de la barre de navigation
 
 class IdentificationScreen extends StatefulWidget {
   @override
@@ -11,10 +11,10 @@ class IdentificationScreen extends StatefulWidget {
 }
 
 class _IdentificationScreenState extends State<IdentificationScreen> {
-  late ClassifierService _classifierService;
+  late ClassifierService _classifierService; // Service de classification
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
-  XFile? _imageFile;  // Variable pour stocker l'image capturée
+  XFile? _imageFile; // Variable pour stocker l'image capturée
 
   bool _isCameraInitialized = false;
   bool _isPhotoTaken = false; // Pour savoir si une photo a été prise
@@ -22,15 +22,17 @@ class _IdentificationScreenState extends State<IdentificationScreen> {
   @override
   void initState() {
     super.initState();
-    _initializeCamera();  // Initialiser la caméra au démarrage
-    _classifierService = ClassifierService();  // Initialiser le service de classification
-    _classifierService.loadModel();  // Charger le modèle de classification
+    _initializeCamera(); // Initialiser la caméra au démarrage
+    _classifierService =
+        ClassifierService(); // Initialiser le service de classification
+    _classifierService.loadModel(); // Charger le modèle de classification
   }
 
   // Initialiser la caméra
   void _initializeCamera() async {
-    final cameras = await availableCameras();  // Récupérer les caméras disponibles
-    final firstCamera = cameras.first;  // Choisir la première caméra
+    final cameras =
+        await availableCameras(); // Récupérer les caméras disponibles
+    final firstCamera = cameras.first; // Choisir la première caméra
 
     _controller = CameraController(firstCamera, ResolutionPreset.high);
     _initializeControllerFuture = _controller.initialize();
@@ -41,7 +43,7 @@ class _IdentificationScreenState extends State<IdentificationScreen> {
     // Attendre la fin de l'initialisation avant de changer l'état
     _initializeControllerFuture.then((_) {
       setState(() {
-        _isCameraInitialized = true;  // La caméra est prête
+        _isCameraInitialized = true; // La caméra est prête
       });
     }).catchError((e) {
       print('Erreur lors de l\'initialisation de la caméra : $e');
@@ -51,12 +53,13 @@ class _IdentificationScreenState extends State<IdentificationScreen> {
   // Prendre une photo
   void _takePicture() async {
     try {
-      await _initializeControllerFuture;  // S'assurer que la caméra est prête avant de prendre la photo
-      final image = await _controller.takePicture();  // Prendre une photo
+      await _initializeControllerFuture; // S'assurer que la caméra est prête avant de prendre la photo
+      final image = await _controller.takePicture(); // Prendre une photo
+      _classifierService.classifyImage(image); // Classer l'image capturée
 
       setState(() {
-        _imageFile = image;  // Sauvegarder l'image capturée dans une variable
-        _isPhotoTaken = true;  // Marquer qu'une photo a été prise
+        _imageFile = image; // Sauvegarder l'image capturée dans une variable
+        _isPhotoTaken = true; // Marquer qu'une photo a été prise
       });
     } catch (e) {
       print('Error taking picture: $e');
@@ -66,15 +69,15 @@ class _IdentificationScreenState extends State<IdentificationScreen> {
   // Revenir à la vue de la caméra pour prendre une autre photo
   void _resetCamera() {
     setState(() {
-      _isPhotoTaken = false;  // Remettre à l'état initial, caméra visible
-      _imageFile = null;  // Réinitialiser l'image capturée
+      _isPhotoTaken = false; // Remettre à l'état initial, caméra visible
+      _imageFile = null; // Réinitialiser l'image capturée
     });
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();  // Libérer la caméra lorsqu'on quitte la page
+    _controller.dispose(); // Libérer la caméra lorsqu'on quitte la page
   }
 
   @override
@@ -89,7 +92,8 @@ class _IdentificationScreenState extends State<IdentificationScreen> {
                   Positioned.fill(
                     child: Image.file(
                       File(_imageFile!.path), // Affiche l'image capturée
-                      fit: BoxFit.cover, // S'assurer que l'image occupe tout l'espace
+                      fit: BoxFit
+                          .cover, // S'assurer que l'image occupe tout l'espace
                     ),
                   )
                 else
@@ -103,31 +107,39 @@ class _IdentificationScreenState extends State<IdentificationScreen> {
                     top: 20, // Positionner la flèche en haut à gauche
                     left: 10,
                     child: IconButton(
-                      icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
-                      onPressed: _resetCamera,  // Remet la caméra visible
+                      icon:
+                          Icon(Icons.arrow_back, color: Colors.white, size: 30),
+                      onPressed: _resetCamera, // Remet la caméra visible
                     ),
                   ),
 
                 // Bouton de capture au centre en bas (seulement si la caméra est active)
                 if (!_isPhotoTaken)
                   Positioned(
-                    bottom: 90,  // Positionné juste au-dessus de la barre de navigation
-                    left: MediaQuery.of(context).size.width * 0.5 - 35,  // Centrage horizontal
+                    bottom:
+                        90, // Positionné juste au-dessus de la barre de navigation
+                    left: MediaQuery.of(context).size.width * 0.5 -
+                        35, // Centrage horizontal
                     child: FloatingActionButton(
-                      onPressed: _takePicture,  // Prendre une photo
-                      child: Icon(Icons.camera_alt, size: 30),  // Icône de capture
+                      onPressed: _takePicture, // Prendre une photo
+                      child:
+                          Icon(Icons.camera_alt, size: 30), // Icône de capture
                       backgroundColor: Colors.white,
-                      foregroundColor: Colors.black, // Meilleur contraste sur un fond blanc
-                      elevation: 10,  // Ajoute une ombre pour un effet plus visible
+                      foregroundColor:
+                          Colors.black, // Meilleur contraste sur un fond blanc
+                      elevation:
+                          10, // Ajoute une ombre pour un effet plus visible
                     ),
                   ),
               ],
             )
-          : Center(child: CircularProgressIndicator()),  // Afficher un indicateur de chargement pendant l'initialisation
+          : Center(
+              child:
+                  CircularProgressIndicator()), // Afficher un indicateur de chargement pendant l'initialisation
 
       // La barre de navigation en bas sans espace
       bottomNavigationBar: BottomNavBar(
-        selectedIndex: 1,  // Indique que l'écran actif est "Identification"
+        selectedIndex: 1, // Indique que l'écran actif est "Identification"
       ),
     );
   }
--- fishnet/lib/services/classifier_service.dart
+++ fishnet/lib/services/classifier_service.dart
@@ -1,18 +1,111 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:image/image.dart' as img;
+import 'package:path_provider/path_provider.dart';
 
 class ClassifierService {
-    late Interpreter _interpreter;
+  late Interpreter _interpreter;
+  late List<int> inputTensorShape;
+  late int inputTensorSize;
+  late List<List<int>> outputTensorShape;
+  late List<int> outputTensorSizes;
+  late Map<String, dynamic> labels;
 
-    static const String modelFile = 'lib/assets/model.tflite';
+  static const String modelFile = 'lib/assets/resnet-11.tflite';
+  static const List<double> mean = [0.485, 0.456, 0.406];
+  static const List<double> std = [0.229, 0.224, 0.225];
 
-    Future<void> loadModel() async {
-        try {
-            _interpreter = await Interpreter.fromAsset(modelFile);
-            print(_interpreter.getInputTensor(0).shape);
-        } catch (e) {
-            print("Erreur lors du chargement du modèle : $e");
-        }
+  Future<void> loadModel() async {
+    try {
+      _interpreter = await Interpreter.fromAsset(modelFile);
+      inputTensorShape = _interpreter.getInputTensor(0).shape;
+      inputTensorSize = inputTensorShape.reduce((a, b) => a * b);
+      outputTensorShape = [];
+      outputTensorSizes = [];
+      _interpreter.getOutputTensors().forEach((outputTensor) {
+        outputTensorShape.add(outputTensor.shape);
+        outputTensorSizes.add(outputTensor.shape.reduce((a, b) => a * b));
+      });
+      _interpreter.allocateTensors();
+
+      print(
+          "INPUT SHAPE: $inputTensorShape, SIZE: $inputTensorSize, NUM OUTPUT TENSORS: ${outputTensorShape.length}");
+      for (var outputShape in outputTensorShape) {
+        print("OUTPUT SHAPE: $outputShape");
+      }
+      String jsonString = await rootBundle.loadString('lib/assets/labels.json');
+      labels = await json.decode(jsonString);
+    } catch (e) {
+      print("Erreur lors du chargement du modèle : $e");
     }
+  }
 
-    Interpreter get interpreter => _interpreter;
+  Future<void> classifyImage(XFile xfile) async {
+    try {
+      // ByteData data = await rootBundle.load("lib/assets/test_image.png");
+      // List<int> bytes = data.buffer.asUint8List();
+
+      // Directory tempDir = await getTemporaryDirectory();
+      // File file = File('${tempDir.path}/test_image.png');
+
+      // await file.writeAsBytes(bytes);
+
+      // xfile = XFile(file.path);
+      Uint8List bytes2 = await xfile.readAsBytes();
+      img.Image? image = img.decodeImage(Uint8List.fromList(bytes2));
+
+      img.Image? resizedImage = img.copyResize(image!,
+          width: inputTensorShape[inputTensorShape.length - 2],
+          height: inputTensorShape[inputTensorShape.length - 1]);
+      Float32List inputBytes = Float32List(inputTensorSize);
+
+      int pixelIndex = 0;
+      final sliceSize = inputTensorShape[inputTensorShape.length - 2] *
+          inputTensorShape[inputTensorShape.length - 1];
+      for (int y = 0; y < resizedImage.height; y++) {
+        for (int x = 0; x < resizedImage.width; x++) {
+          final pixel = resizedImage.getPixel(x, y); // Récupère un objet Pixel
+          inputBytes[pixelIndex] = ((pixel.b / 255.0) - mean[0]) / std[0];
+          inputBytes[sliceSize + pixelIndex] =
+              ((pixel.g / 255.0) - mean[1]) / std[1];
+          inputBytes[2 * sliceSize + pixelIndex] =
+              ((pixel.r / 255.0) - mean[2]) / std[2];
+
+          pixelIndex++;
+        }
+      }
+
+      final input = inputBytes.reshape(inputTensorShape);
+
+      final output = {
+        0: Float32List(outputTensorSizes[0]).reshape(outputTensorShape[0]),
+        1: Float32List(outputTensorSizes[1]).reshape(outputTensorShape[1]),
+      };
+
+      _interpreter.runForMultipleInputs([input], output);
+
+      print("FINISHED INFERENCE");
+      final probabilities = output[1]![0];
+      double max = 0;
+      int maxIndex = 0;
+      for (int i = 0; i < probabilities.length; i++) {
+        if (probabilities[i] > max) {
+          max = probabilities[i];
+          maxIndex = i;
+        }
+      }
+      print("${maxIndex.toString()}, $max");
+    } catch (e) {
+      print("Erreur lors de la classification de l'image : $e");
+      return;
+    }
+  }
+
+  Interpreter get interpreter => _interpreter;
 }
